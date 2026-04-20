d7a6429fc7de300753b1cf1b2f0d62a9329b5a9d
MaitySoumitra <soumitra.ahaansoftware@gmail.com>
2026-04-20 19:43:07 +0530

    file lots of issue

--- main.py
+++ main.py
@@ -1,53 +1,60 @@
 from fastapi import FastAPI
 from model import Product
+from database import session, engine
+import database_models
+
+database_models.Base.metadata.create_all(bind=engine)
+
 app=FastAPI()
 
-
-@app.get('/')
-
-def greet():
-    return "Welcome to Our WebSite"
-
 products=[
-    Product(id=1, name="phone", description="Base model", price=199, quantity=10 ),
-    Product(id=2, name="laptop", description="dell laptop", price=499, quantity=6),
-    Product(id=3, name="watch", description="Dial watch", price=99, quantity=20 ),
-    Product(id=4, name="Tv", description="LED Tv", price=299, quantity=3 ),
+    Product(id=1, name="Phone", description="Redmi Phone", price=149, quantity=13),
+    Product(id=2, name="Tv", description="32 inch LCD Tv", price=299, quantity=6),
+    Product(id=3, name="Watch", description="Smart Watch", price=99, quantity=15),
+    Product(id=4, name="Earphone", description="boat earphone", price=79, quantity=20),
+    Product(id=5, name="Laptop", description="Hp Laptop", price=999, quantity=5)
 ]
 
-@app.get('/products')
+@app.get("/")
+def home_practice():
+    return "Welcome to home page"
 
-def get_all_product():
+@app.get("/product")
+
+def get_product():
+    db=session()
+    db.query()
     return products
+    
 
-@app.get('/products/{id}')
+@app.get("/products/{id}")
 
-def get_product_byId(id:int):
+def get_productByID(id: int):
     for product in products:
         if product.id==id:
             return product
-    return "product not found"
+        
+    return "not fetch product"
 
-@app.post('/product')
-
-def create_product(product: Product):
-    products.append(product)
-    return product
-
-@app.put('/product')
+@app.put("/products")
 
 def update_product(id:int, product:Product):
     for i in range(len(products)):
         if products[i].id==id:
-            products[i]= product
-            return "product updated successfully"
-    return "No product found"
+            products[i]=product
+            return "product updated"
+    return "Product not update"
 
-@app.delete('/product')
+@app.post("/products")
+def create_product(product:Product):
+    products.append(product)
+    return product
+    
+
+@app.delete("/products")
 def delete_product(id:int):
     for i in range(len(products)):
         if products[i].id==id:
             del products[i]
-            return "product deleted successfully"
+            return "product deleted"
     return "product not deleted"
-        
